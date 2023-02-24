3466b80b83266eeae52aa5e8c5b351667138e6a9
Menekam Rudy <menekamrudy@gmail.com>
2023-02-24 15:05:32 +0100

    Add event functionalities

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,36 +1,60 @@
-import React from 'react';
-import KeyFunctions from './key-functions';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const initialState = {
+    total: 0,
+    next: '',
+    operation: '',
+  };
+  const [state, setState] = useState(initialState);
+  const { total, operation, next } = state;
+  const onClickHandler = (event) => {
+    const TargetValue = event.target.value;
+    const updateState = calculate(state, TargetValue);
+    setState(updateState);
+  };
   return (
     <div className="calculator">
       <div className="calculator-input">
-        <div className="result"><span>0</span></div>
+        <div className="result">
+          {total}
+          {' '}
+          {operation}
+          {' '}
+          {next}
+          {' '}
+        </div>
       </div>
       <div className="calculator-keypad">
-        <KeyFunctions />
         <div className="row">
-          <button type="button" className="keys-numbers">9</button>
-          <button type="button" className="keys-numbers">8</button>
-          <button type="button" className="keys-numbers">7</button>
-          <button type="button" className="keys-operators">x</button>
+          <button onClick={onClickHandler} type="button" className="keys-function" value="AC">AC</button>
+          <button onClick={onClickHandler} type="button" className="keys-function" value="+/-">+/-</button>
+          <button onClick={onClickHandler} type="button" className="keys-function" value="%">%</button>
+          <button onClick={onClickHandler} type="button" className="keys-operators" value="÷">÷</button>
         </div>
         <div className="row">
-          <button type="button" className="keys-numbers">6</button>
-          <button type="button" className="keys-numbers">5</button>
-          <button type="button" className="keys-numbers">4</button>
-          <button type="button" className="keys-operators">-</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="9">9</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="8">8</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="7">7</button>
+          <button onClick={onClickHandler} type="button" className="keys-operators" value="x">x</button>
         </div>
         <div className="row">
-          <button type="button" className="keys-numbers">3</button>
-          <button type="button" className="keys-numbers">2</button>
-          <button type="button" className="keys-numbers">1</button>
-          <button type="button" className="keys-operators">+</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="6">6</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="5">5</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="4">4</button>
+          <button onClick={onClickHandler} type="button" className="keys-operators" value="-">-</button>
         </div>
         <div className="row">
-          <button type="button" className="keys-numbers key-zero">0</button>
-          <button type="button" className="keys-numbers">.</button>
-          <button type="button" className="keys-operators">=</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="3">3</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="2">2</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value="1">1</button>
+          <button onClick={onClickHandler} type="button" className="keys-operators" value="+">+</button>
+        </div>
+        <div className="row">
+          <button onClick={onClickHandler} type="button" className="keys-numbers key-zero" value="0">0</button>
+          <button onClick={onClickHandler} type="button" className="keys-numbers" value=".">.</button>
+          <button onClick={onClickHandler} type="button" className="keys-operators" value="=">=</button>
         </div>
       </div>
     </div>
